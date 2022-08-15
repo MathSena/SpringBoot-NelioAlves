57aee3d00c77f60cf2eb22ef77fb3dc7b2978da2
Matheus Sena <MathSena07@hotmail.com>
2022-08-15 19:14:57 -0300

    Criando o profile de desenvolvimento

--- src/main/java/com/mathsena/cursomc/CursomcApplication.java
+++ src/main/java/com/mathsena/cursomc/CursomcApplication.java
@@ -1,37 +1,9 @@
 package com.mathsena.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.mathsena.cursomc.domain.Categoria;
-import com.mathsena.cursomc.domain.Cidade;
-import com.mathsena.cursomc.domain.Cliente;
-import com.mathsena.cursomc.domain.Endereco;
-import com.mathsena.cursomc.domain.Estado;
-import com.mathsena.cursomc.domain.ItemPedido;
-import com.mathsena.cursomc.domain.Pagamento;
-import com.mathsena.cursomc.domain.PagamentoComBoleto;
-import com.mathsena.cursomc.domain.PagamentoComCartao;
-import com.mathsena.cursomc.domain.Pedido;
-import com.mathsena.cursomc.domain.Produto;
-import com.mathsena.cursomc.domain.enums.EstadoPagamento;
-import com.mathsena.cursomc.domain.enums.TipoCliente;
-import com.mathsena.cursomc.repositories.CategoriaRepository;
-import com.mathsena.cursomc.repositories.CidadeRepository;
-import com.mathsena.cursomc.repositories.ClienteRepository;
-import com.mathsena.cursomc.repositories.EnderecoRepository;
-import com.mathsena.cursomc.repositories.EstadoRepository;
-import com.mathsena.cursomc.repositories.ItemPedidoRepository;
-import com.mathsena.cursomc.repositories.PagamentoRepository;
-import com.mathsena.cursomc.repositories.PedidoRepository;
-import com.mathsena.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
--- src/main/java/com/mathsena/cursomc/services/DBService.java
+++ src/main/java/com/mathsena/cursomc/services/DBService.java
@@ -52,6 +52,7 @@ public class DBService {
         Categoria cat7 = new Categoria(null, "Higiêne");
 
 
+
         Produto p1 = new Produto(null, "Computador", 2000);
         Produto p2 = new Produto(null, "Impressora", 1000);
         Produto p3 = new Produto(null, "Mouse", 80);
